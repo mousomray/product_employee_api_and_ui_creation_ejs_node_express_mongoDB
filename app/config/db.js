e349c9692e195ae72f1447f23394b85aad0e89db
mousomray <mousomray02@gmail.com>
2024-10-31 10:53:45 +0530

    I add a comment in db.js file

--- app/config/db.js
+++ app/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect(process.env.MONGO_URL) // This is store in .env file
         console.log('MongoDB connected')
     } catch (error) {
         console.log('mongodb error', error)
