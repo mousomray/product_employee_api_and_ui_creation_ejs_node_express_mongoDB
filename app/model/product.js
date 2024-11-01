e4c7e15b2b96724f6d523055af11c38184f8158f
mousomray <mousomray02@gmail.com>
2024-11-01 19:18:23 +0530

    I make some changes in product schema

--- app/model/product.js
+++ app/model/product.js
@@ -4,26 +4,35 @@ const Schema = mongoose.Schema;
 const ProductSchema = new Schema({
     p_name: {
         type: String,
-        required: true,
+        required: "p_name is required",
         minlength: [3, 'p_name must be at least 3 characters']
     },
     p_size: {
         type: [String], // Array of strings for sizes
-        required: true,
-        minlength: [5, 'p_size must be at least 3 characters long']
+        validate: {
+            validator: function (v) {
+                return v.length > 0; // Must have at least one size
+            },
+            message: "Please select atleast one size."
+        }
     },
     p_color: {
         type: [String], // Array of strings for colors
-        required: true,
-        minlength: [5, 'p_color must be at least 3 characters long']
+        validate: {
+            validator: function (v) {
+                return v.length > 0; // Must have at least one color
+            },
+            message: "Please select atleast one color"
+        }
     },
+
     price: {
         type: Number,
-        required: true,
+        required: "Price is Required",
     },
     image: {
         type: String,
-        default: 'https://t3.ftcdn.net/jpg/05/25/55/06/360_F_525550698_pcSOrKvA81oMHeEgI1e6MJHn1NIzWREd.jpg'
+        required: "Put an image URL from google it is required"
     },
 }, { timestamps: true });
 
