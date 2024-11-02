a9a898902ae2e3d5926180e1599acab182a296dd
mousomray <mousomray02@gmail.com>
2024-11-02 09:59:51 +0530

    I make changes in employee model

--- app/model/employee.js
+++ app/model/employee.js
@@ -4,37 +4,37 @@ const Schema = mongoose.Schema;
 const EmployeeSchema = new Schema({
     name: {
         type: String,
-        required: true,
+        required: "Employee name is Required",
         minlength: [3, 'Name must be at least 3 characters long']
     },
     email: {
         type: String,
-        required: true,
+        required: "Employee email is Required",
         match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Email address should follow the format: abc@gmail.com']
     },
     phone: {
         type: Number,
-        required: true,
+        required: "Phone number is Required",
         min: [1000000000, 'Phone number must be exactly 10 digits'],
         max: [9999999999, 'Phone number must be exactly 10 digits']
     },
     city: {
         type: String,
-        required: true,
+        required: "City is Required",
         minlength: [3, 'City must be at least 3 characters long']
     },
     pin: {
         type: Number,
-        required: true
+        required: "Put a valid pincode"
     },
     position: {
         type: String,
-        required: true,
+        required: "Enter your work position",
         minlength: [3, 'Position must be at least 3 characters long']
     },
     image: {
         type: String,
-        default: 'https://t3.ftcdn.net/jpg/05/25/55/06/360_F_525550698_pcSOrKvA81oMHeEgI1e6MJHn1NIzWREd.jpg'
+        required: "Put an image URL from google it is required"
     },
 
 }, { timestamps: true }); // timestamps show create date and update date
